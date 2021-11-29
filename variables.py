94eb648a14eafcc8cc9e730d8211a852b5ceb868
chickenshifu <khlrtbs@gmail.com>
2021-11-29 13:41:40 +0100

    started unfollowing all gitignore files

--- variables.py
+++
@@ -1,8 +0,0 @@
-import os
-
-mailhandler_usr = os.getenv('MAILHANDLER_USR')
-mailhandler_pwd = os.getenv('MAILHANDLER_PWD')
-mailhandler_recipient = os.getenv('MAILHANDLER_RECIPIENT')
-
-print(f'Sending mail address: {mailhandler_usr}')
-print(f'Receiving mail address: {mailhandler_recipient}')
